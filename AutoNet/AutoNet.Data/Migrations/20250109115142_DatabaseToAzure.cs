34be25fababd38ffa3c955265dc760a7c09759c4
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2025-01-09 13:55:14 +0200

    DataBase to Azure Migration

---
+++ AutoNet/AutoNet.Data/Migrations/20250109115142_DatabaseToAzure.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoNet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseToAzure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_FileToDatabases_CarId",
+                table: "FileToDatabases",
+                column: "CarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FileToDatabases_Cars_CarId",
+                table: "FileToDatabases",
+                column: "CarId",
+                principalTable: "Cars",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FileToDatabases_Cars_CarId",
+                table: "FileToDatabases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FileToDatabases_CarId",
+                table: "FileToDatabases");
+        }
+    }
+}
