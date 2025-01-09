04afa6086121d5d4de7c626661d6a1c422450fec
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2025-01-09 13:03:09 +0200

    Publish to Azure

---
+++ AutoNet/AutoNet.Core/Domain/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoNet.Core.Domain
+{
+    public class EmailSettings
+    {
+        public string SenderEmail { get; set; }
+        public string SenderPassword { get; set; }
+        public string SmtpHost { get; set; }
+        public string SmtpPort { get; set; }
+        public bool EnableSsl { get; set; }
+    }
+}
